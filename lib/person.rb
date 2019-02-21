fef0af05a0a4c1256a60116e4189d561b65a81ce
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-21 22:29:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -82,6 +82,8 @@ class Person
         person.happiness -= 2
       return "blah blah partisan blah lobbyist"
     elsif topic == "weather"
+        self.happiness += 1
+        person.happiness += 1
       return "blah blah sun blah rain"
 
   end
