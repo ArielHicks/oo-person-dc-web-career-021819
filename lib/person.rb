6214e278aae825cd81e145ea59d6b11e9a816cea
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-21 20:46:53 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,9 +6,11 @@
 # state if they are happy and/or clean
 
 class Person
-
-attr_reader :name
 # First you need to create a person class that is initialized with a name that cannot be changed.
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+  end
 
 end
