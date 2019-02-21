0ca3cf00f2e6ccc4a6fb882817236c85a1d7a616
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-21 20:44:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 # your code goes here
+# get paid/receive payments
+# take a bath
+# call a friend
+# start a conversation
+# state if they are happy and/or clean
+
+class Person
+
+attr_reader :name
+# First you need to create a person class that is initialized with a name that cannot be changed.
+
+
+end
