ec91b174d341cfcd2952d2adcee6cfda223ae68b
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-21 22:12:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -73,7 +73,7 @@ class Person
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
-    return "Hi #{friend}! It's #{self.name}. How are you?"
+    return "Hi #{friend}! It's #{self}. How are you?"
 
   end
 
